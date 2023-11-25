a1ec5e6023d80da982992f5ce074c0bf73dbb958
Romero Yaguana Anthony Joel <romeroyaguanaanthonyjoel@gmail.com>
2023-11-25 16:20:48 -0500

    consulta001-AnthonyJoelRY

---
+++ consulta001/consulta001.py
@@ -0,0 +1,5 @@
+nombre_del_usuario = input("Ingrese su nombre: ")
+print("Hola,",nombre_del_usuario,",ingrese los datos solicitado")
+apellido_del_usuario = input("Ingrese su apellido: ")
+edad_del_usuario = input("Ingrese su edad: ")
+print ("Estos datos pertenecen a",nombre_del_usuario,apellido_del_usuario,"con edad de",edad_del_usuario,"años")
